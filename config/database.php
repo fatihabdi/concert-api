66a628926199f8c57bb139d6c2aac2a9e1c1b7c8
fatihabdi <dotaawtf23@gmail.com>
2023-06-12 00:05:15 +0700

    aa

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => 'isrgrootx1.pem',
             ]) : [],
